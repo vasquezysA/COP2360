a282df9e8d1d7d396fa04d1a4fc455c34c28841a
vasquezysA <rachel.vasquez@my.pbsc.edu>
2026-02-15 23:02:21 -0500

    Student class assignment

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Student
+{
+    public string Name = "";
+    public string Major ="";
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and my major is {Major} .");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student();
+
+        student1.Name = "Rachel Vasquez";
+        student1.Major = "Information Technology";
+
+        student1.Introduce();
+    }
+}
